e7fc6d0219c375164ed90f5448ff4464303074e5
Mydhili-GP <mydhili2632@gmail.com>
2021-06-05 13:24:16 +0530

    first esclipe

---
+++ CoreJavaPractice/src/basic/FirstProgramInEclipse.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class FirstProgramInEclipse {
+
+	public static void main(String[] args) {
+		System.out.println("First Program");
+		System.out.println(args.length);
+		System.out.println(args[0] + args[1]);
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println(a + b);
+
+	}
+
+}
